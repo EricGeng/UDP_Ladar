2ac4e1cf22092d92c719d7700ebe318312685831
EricGeng <370292309@qq.com>
2019-03-26 10:33:01 +0800

    first UDP demo

---
+++ ConsoleApplication1/ConsoleApplication1/Program1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static Socket client;
+        static void Main(string[] args)
+        {
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            client.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.10"), 2014));
+            Thread t = new Thread(sendMsg);
+            t.Start();
+            Thread t2 = new Thread(receiveMsg);
+            t2.Start();
+            Console.WriteLine("客户端开启");
+           
+           /* 
+           */
+         }
+        //给特定的ip的主机端口发生送数据包
+        static void sendMsg()
+        {
+            EndPoint point = new IPEndPoint(IPAddress.Parse("192.168.0.3"), 2015);
+            while (true)
+            {
+                string msg = Console.ReadLine();
+                client.SendTo(Encoding.UTF8.GetBytes(msg), point);
+            }
+        }
+        //接受发送给本机ip特定端口的数据包         
+        static void receiveMsg()
+        {
+            while (true)
+            {  
+                EndPoint point = new IPEndPoint(IPAddress.Any, 0);
+                byte[] buffer = new byte[2048];
+                int length = client.ReceiveFrom(buffer, ref point);
+                string message = Encoding.UTF8.GetString(buffer, 0, length);
+                Console.WriteLine(point.ToString() + message);
+                 string i = Console.ReadLine();
+                if (i == "s")
+                {
+                    break;
+                }
+            }
+        }
+     }
+}
---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+            string i = Console.ReadLine();
+                /* if(i=="1")
+                 {
+                     Console.WriteLine("true");
+                    //onsole.ReadKey();
+                 }
+                 else
+                 {
+                     Console.WriteLine("false");
+                     //nsole.ReadKey();              
+                     }*/
+             int num = Int32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+                Console.WriteLine(num);
+            }
+            
+        }
+    }
+}

eeee39df904c83941535bdbd5c67027c620f9061
EricGeng <370292309@qq.com>
2019-04-03 23:47:37 +0800

    change matlab scripts

--- dataseeing.m
+++ dataseeing.m
@@ -1,14 +1,17 @@
 % 数据可视化
 clear;
 clc;
-m=load("C:\Users\EricGeng\Desktop\23.txt");     %单独读取文件全部数据
-data=ones(32,3);
+m=load("C:\Users\radar\Desktop\全部数据.txt");   %单独读取文件全部数据
+n=size(m);
+n=n(1)/4;
+data=ones(n,4);
 k=1;
-for i=1:32
+for i=1:n
     data(i,1)=m(k);
     data(i,2)=m(k+1);
     data(i,3)=m(k+2);
-    k=k+3;
+    data(i,4)=m(k+3);
+    k=k+4;
 end
 x=data(:,1);
 y=data(:,2);
